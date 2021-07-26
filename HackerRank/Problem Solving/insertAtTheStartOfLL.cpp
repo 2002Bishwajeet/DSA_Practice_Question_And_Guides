34192fb22a984110d1bdadca79496837cd76ab52
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-07-26 16:23:49 +0530

    added two new LL programs

---
+++ HackerRank/Problem Solving/insertAtTheStartOfLL.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+int main()
+{
+    //  insertiona at the beginning of LL
+    int n;
+    cin >> n;
+    int ar[n];
+    node *head = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+        node *temp = new node;
+        temp->data = ar[i];
+        temp->next = head;
+        head = temp;
+    }
+    //  display the data in the LL
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    delete temp;
+    delete head;
+
+    return 0;
+}
