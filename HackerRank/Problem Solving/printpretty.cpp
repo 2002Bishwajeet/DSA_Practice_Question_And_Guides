e577e01c6ce32fd6f7cb822db82b094bcd440434
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-08-21 23:45:40 +0530

    450Ds questions added

---
+++ HackerRank/Problem Solving/printpretty.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        double A;
+        cin >> A;
+        double B;
+        cin >> B;
+        double C;
+        cin >> C;
+    }
+    return 0;
+}
